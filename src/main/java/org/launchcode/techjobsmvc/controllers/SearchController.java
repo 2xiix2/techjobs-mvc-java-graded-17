96a1077bddc095fa07b7387d189c7b2c1faa8da6
2xiix2 <122521905+2xiix2@users.noreply.github.com>
2023-11-13 15:31:51 -0600

    added display controller for SearchContr penultimate push

--- src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
@@ -1,9 +1,14 @@
 package org.launchcode.techjobsmvc.controllers;
 
+import jakarta.websocket.server.PathParam;
+import org.launchcode.techjobsmvc.models.Job;
+import org.launchcode.techjobsmvc.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.launchcode.techjobsmvc.controllers.ListController.columnChoices;
 
@@ -23,5 +28,18 @@ public class SearchController {
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
 
+    @PostMapping(value="results")
+    public String displaySearchResults(@RequestParam String searchTerm, @RequestParam String searchType, Model model) {
+        ArrayList<Job> jobs;
+        if (searchTerm.equals("all")) {
+            jobs = JobData.findAll();
+            model.addAttribute("searchTerm", searchTerm);
+        } else {
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+            model.addAttribute("searchTerm", searchType);
+        }
+        model.addAttribute("jobs", jobs);
+        return "search";
+    }
 }
 
